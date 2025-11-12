9d0fb8c4c71328893f8b90f4557396e1eec54ac3
furifurirunan <kevin.arivas1@gmail.com>
2025-11-11 21:33:55 -0300

    Add auto migration and auto superuser script for Railway

--- auto_migrate.py
+++ auto_migrate.py
@@ -1,11 +1,27 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import django
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
+# Configuración de Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SalonVuala.settings')
-application = get_wsgi_application()
+django.setup()
 
 print("📦 Ejecutando migraciones en Railway...")
 call_command('makemigrations', interactive=False)
 call_command('migrate', interactive=False)
 print("✅ Migraciones completadas correctamente.")
+
+# Crear superusuario automáticamente
+User = get_user_model()
+username = "Drumontt"
+email = ""
+password = "1234"  # ⚠️ Puedes cambiarla luego en /admin
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"👑 Superusuario '{username}' creado correctamente.")
+else:
+    print(f"✅ El superusuario '{username}' ya existe.")
+
+print("🚀 Configuración completada.")
