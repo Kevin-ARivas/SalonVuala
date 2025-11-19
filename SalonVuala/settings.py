868f764f8eaf47ef5144c2c9e924b16c223975a2
furifurirunan <kevin.arivas1@gmail.com>
2025-11-18 23:15:20 -0300

    ahora si x2

--- SalonVuala/settings.py
+++ SalonVuala/settings.py
@@ -151,11 +151,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ============================================
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")  # postmaster@mg.salonvuala.cl
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")  # API SMTP password
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
